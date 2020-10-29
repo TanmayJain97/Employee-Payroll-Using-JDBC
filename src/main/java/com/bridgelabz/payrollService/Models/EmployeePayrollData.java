6df7f5f8debe46206a6ee1acc87f6f5d4ee11470
TanmayJain97 <mail.tanmay.jain@gmail.com>
2020-10-29 22:45:27 +0530

    Add: Equals Method

--- src/main/java/com/bridgelabz/payrollService/Models/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/payrollService/Models/EmployeePayrollData.java
@@ -47,4 +47,30 @@ public class EmployeePayrollData {
 	public String toString() {
 		return "Created new Employee Data";
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmployeePayrollData other = (EmployeePayrollData) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		return true;
+	}	
 }
